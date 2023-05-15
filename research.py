3ec321a108591c405daed1596bd917627914c7f9
BelkanWar <challenger1501@gmail.com>
2023-05-16 07:37:23 +0800

    2023-5-16 progress

--- research.py
+++ research.py
@@ -1,11 +1,17 @@
+'''
+https://peaceful0907.medium.com/time-series-prediction-lstm%E7%9A%84%E5%90%84%E7%A8%AE%E7%94%A8%E6%B3%95-ed36f0370204
+'''
+
 import utils
 import pandas as pd
 import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # parameters
 TRAIN_LENGTH = 100
 LOOKBACK_LENGTH = 10
-EPOCH = 20
+EPOCH = 3000
 
 data = pd.read_csv("data/AirPassengers.csv")
 data['Month'] = pd.to_datetime(data['Month'], format='%Y-%m')
@@ -24,7 +30,32 @@ model = utils.training(model, loader, EPOCH)
 
 
 with torch.inference_mode():
-    series = data['#Passengers'].tolist()
+    plot = {
+        'value':data['#Passengers'].tolist(),
+        'type':['ground truth']*data.shape[0],
+        'X':[i for i in range(data.shape[0])]}
+    
+    series = data['#Passengers'].tolist()[:TRAIN_LENGTH]
     tensor = torch.tensor(series).reshape(1, -1, 1)
-    Z = model(tensor)
+    predict = model(tensor).flatten().tolist()
+    
+    plot['value'] += predict
+    plot['type'] += ['fit']*(len(predict)-1) + ['predict']
+    plot['X'] += [i for i in range(1, TRAIN_LENGTH+1)]
+    series.append(predict[-1])
+    
+    for X in range(TRAIN_LENGTH+1, data.shape[0]):
+        tensor = torch.tensor(series).reshape(1, -1, 1)
+        predict = model(tensor).flatten().tolist()
+
+        plot['value'].append(predict[-1])
+        plot['type'].append('predict')
+        plot['X'].append(X)
+        series.append(predict[-1])
+        print(X, len(series))
+        
+plt.plot()
+sns.lineplot(data=plot, x='X',y='value',hue='type')
+plt.savefig("img/continus_prediction.png")
+plt.close()
 
--- utils.py
+++ utils.py
@@ -5,14 +5,16 @@ class continus_Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
         RNN_DIM = 50
+        LINEAR0_OUT_DIM = 50
         LINEAR1_OUT_DIM = 25
         LINEAR2_OUT_DIM = 12
         LINEAR3_OUT_DIM = 6
         LINEAR4_OUT_DIM = 1
         
         self.rnn = torch.nn.GRU(
-            input_size=1, hidden_size=RNN_DIM, num_layers=3, batch_first=True, 
+            input_size=1, hidden_size=RNN_DIM, num_layers=6, batch_first=True, 
             bidirectional=False)
+        # self.linear0 = torch.nn.Linear(RNN_DIM*2, LINEAR0_OUT_DIM)
         self.linear1 = torch.nn.Linear(RNN_DIM, LINEAR1_OUT_DIM)
         self.linear2 = torch.nn.Linear(LINEAR1_OUT_DIM, LINEAR2_OUT_DIM)
         self.linear3 = torch.nn.Linear(LINEAR2_OUT_DIM, LINEAR3_OUT_DIM)
@@ -20,6 +22,7 @@ class continus_Model(torch.nn.Module):
         
     def forward(self, x_input):
         x_input, ht = self.rnn(x_input)
+        # x_input = F.relu(self.linear0(x_input))
         x_input = F.relu(self.linear1(x_input))
         x_input = F.relu(self.linear2(x_input))
         x_input = self.linear3(x_input)
@@ -32,12 +35,12 @@ def convert2Loader(data_X, data_Y):
         torch.utils.data.TensorDataset(
             torch.tensor(data_Y).unsqueeze(2), 
             torch.tensor(data_X).unsqueeze(2)), 
-        shuffle=True, batch_size=8)
+        shuffle=True, batch_size=200)
     return loader
 
 def training(model, loader, EPOCH):
     model.train()
-    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.002)
     loss_fn = torch.nn.MSELoss()
     
     for epoch in range(EPOCH):
@@ -50,6 +53,7 @@ def training(model, loader, EPOCH):
 
             if step % 10 == 0:
                 print(f"epoch: {epoch} | batch: {step} | loss: {float(loss.data)}")
+                # print(f"x: {x_batch.tolist()[0]} | y: {y_batch.tolist()[0]} | predict: {y_predict.tolist()[0]}")
 
     model.eval()
     return model
