e62127f0a12737f1b964a067ee6a4c8893c92a74
BelkanWar <challenger1501@gmail.com>
2023-05-16 08:11:34 +0800

    lala

--- utils.py
+++ utils.py
@@ -30,6 +30,45 @@ class continus_Model(torch.nn.Module):
 
         return x_input
 
+class Encoder(torch.nn.Module):
+    def __init__(self, input_size=5, hidden_size=128, n_layers=1, dropout=0.3):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        
+        self.lstm = torch.nn.LSTM(input_size, hidden_size, n_layers)
+        
+    def forward(self, x):
+        # x: input batch data, size: [input_seq_len, batch_size, feature_size]
+        output, (hidden, cell) = self.lstm(x)
+        return hidden, cell
+
+
+class Decoder(torch.nn.Module):
+    def __init__(self, output_size=5, hidden_size=128, n_layers=1, dropout=0.3):
+        super().__init__()
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+
+        self.lstm = torch.nn.LSTM(output_size, hidden_size, n_layers)
+        self.linear = torch.nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, hidden, cell):
+        """
+        x size = [batch_size, feature_size]
+        --> x only has two dimensions since the input is batch of last coordinate of observed trajectory
+        so the sequence length has been removed.
+        """
+        # add sequence dimension to x, to allow use of nn.LSTM
+        x = x.unsqueeze(0)  # -->[1, batch_size, feature_size]
+        
+        output, (hidden, cell) = self.lstm(x, (hidden, cell))
+        prediction = self.linear(output)
+
+        return prediction, hidden, cell    
+
+
 def convert2Loader(data_X, data_Y):
     loader = torch.utils.data.DataLoader(
         torch.utils.data.TensorDataset(
